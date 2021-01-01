cea4c4a34102eed9fdbb752d897dc54b6c6df63c
Joe Izzard <Joe@Izzard.me>
2021-01-01 14:48:56 +0000

    feat: added non interactive install scripts for Ansible Use

---
+++ Lib/Installers/SSH-nonint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install SSH!
+echo "[SSH Installer] SSH Installation began..."
+
+# Building Directory Structure
+echo "[SSH Installer] Building Directory Structure"
+mkdir -p ~/.ssh/Keys
+touch ~/.ssh/known_hosts
+
+echo "[SSH Installer] Installation Complete"
